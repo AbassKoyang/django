dd874b1a0df37e03e689503aaa5bf64e6ee19680
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2025-12-14 23:49:43 +0100

    add posts page

---
+++ lesson-01/myproject/posts/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post;
+
+# Create your views here.
+def posts_lists(request):
+    posts = Post.objects.all().order_by('-date');
+    return render(request, 'posts/posts_list.html', {'posts': posts});
+def post_page(request, slug):
+    return HttpResponse(slug);
+
