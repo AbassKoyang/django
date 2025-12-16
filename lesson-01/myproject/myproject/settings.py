152c88f3a95615362f045076200ca0dff26f2ea9
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2025-12-16 14:18:08 +0100

    update allowed hosts

--- lesson-01/myproject/myproject/settings.py
+++ lesson-01/myproject/myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(ynl42k$v34difi&e_y$o0hqvt7bd*n7#n4w^$zq2%yd49x0_z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*"]
 
 
 # Application definition
